43fd9263936724daf03ffc548def0931adba22b6
yekta <ngyekta@gmail.com>
2025-05-20 09:33:58 +0300

    Server

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+
+serve({
+  port: 3001,
+  fetch(req) {
+    console.log("Request received:", req.method, req.url);
+    return new Response("Hello from Bun!");
+  },
+});
+
+console.log("Server is running on http://localhost:3001");
