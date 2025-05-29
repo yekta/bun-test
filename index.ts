5beb87b324cb08394368a46d6d7f9b9e6fb91792
yekta <ngyekta@gmail.com>
2025-05-29 11:16:55 +0300

    Remove port

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
 import { serve } from "bun";
 
 serve({
-  /*   port: 3001, */
   fetch(req) {
     console.log("Request received:", req.method, req.url);
     return new Response("Hello from Bun!");
   },
 });
-
-/* console.log("Server is running on http://localhost:3001"); */
